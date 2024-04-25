d4f813f50254fe443674919b8d9649406405a2ae
Mai <truongkimmai2004@nguyendudalat.edu.vn>
2024-04-25 15:40:51 +0700

    fix error

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,32 +1,32 @@
 const TaskShortener = require('../models/url.model')
-const {URL} = require('url');
+const { URL } = require('url');
 const dns = require('dns')
 const getURL = async (req, res) => {
     // let baseUrl = 'https://localhost:3000/api/shorturl/'
-    const longUrl = await TaskShortener.find({short_url : req.params.id})
-   res.redirect(longUrl[0].original_url)
+    const longUrl = await TaskShortener.find({ short_url: req.params.id })
+    res.redirect(longUrl[0].original_url)
 }
 
-const createShortURL = async (req, res) => {   
-    let valid = false
-    // let urlHost = req.body.url.replace('www.',"")
-    let urlObject = new URL(req.body.url)
-    console.log(urlObject)
-    dns.lookup(urlObject.hostname, async (err) => 
-    {
-        if (err) {
-            res.json({error: 'invalid url'})
-        } else {
-            const url = {}
-            url.original_url = urlObject
-            url.short_url = String(Math.floor(Math.random() * 10000))
-            const shortener = await TaskShortener.create(url)
-            res.json(url)
+const createShortURL = async (req, res) => {
+    if (req.body.url.includes('https://www.')) {
+        let urlObject = new URL(req.body.url)
+        dns.lookup(urlObject.hostname, async (err) => {
+            if (err) {
+                res.json({ error: 'invalid url' })
+            } else {
+                const url = {}
+                url.original_url = new URL(req.body.url)
+                url.short_url = String(Math.floor(Math.random() * 10000))
+                const shortener = await TaskShortener.create(url)
+                res.json(url)
+            }
         }
+        );
+    } else {
+        res.json({ error: 'invalid url' })
     }
-    ); 
 }
-module.exports = { 
+module.exports = {
     getURL,
     createShortURL
 }
